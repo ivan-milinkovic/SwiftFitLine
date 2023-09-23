e213b1a3887602609a43efdf09e624c6722900d0
Ivan Milinkovic <ivanmcode@gmail.com>
2023-09-23 10:20:52 +0200

    Initial Commit

---
+++ SwiftFitLine/SwiftFitLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFitLineApp.swift
+//  SwiftFitLine
+//
+//  Created by Ivan Milinkovic on 23.9.23..
+//
+
+import SwiftUI
+
+@main
+struct SwiftFitLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
